79454925f3a5c106a8481f873937d71c584af146
jayvanput <jayvanput@gmail.com>
2020-12-31 13:34:23 -0500

    Installed middleware and set up basic server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+
+// Set up middleware
+//app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+//app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+    res.json({
+        foo: "bar"
+    });
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+})
