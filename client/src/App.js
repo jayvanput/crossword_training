12d63e8d573568f9c8524086c1fde9bdb1c2f734
jayvanput <jayvanput@gmail.com>
2021-01-01 22:50:13 -0500

    Added mock data

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 function App() {
+
+  let data = {
+    "_id": "5fed66ab0e2012703392b809",
+    "id": 0,
+    "date": "07/28/2014 0:00",
+    "day": "Monday",
+    "difficulty": 6,
+    "direction": "A",
+    "number": 24,
+    "question": "Cutters that cut with the grain",
+    "answer": "RIPSAWS"
+  }
   return (
     <div className="App">
-      hello
+      {data.question}
     </div>
   );
 }
