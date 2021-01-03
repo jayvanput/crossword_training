8cd56c097a060dc8280a1aaeb51ad12060e37062
jayvanput <jayvanput@gmail.com>
2021-01-03 15:03:05 -0500

    Changed api endpoints

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ if (process.env.NODE_ENV === "production") {
 
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
+    app.get('/^((?!(api)).)*$/', (req, res) => {
         res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
     })
 }
