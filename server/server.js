ab711bd15ccb726b7a9e75896727093ad3e694d6
jayvanput <jayvanput@gmail.com>
2021-01-03 13:18:37 -0500

    Added path to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const path = require('path')
 
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
